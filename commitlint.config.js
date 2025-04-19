56e168a1754fbb558079262a665d21347d269342
shein <2226092102@qq.com>
2025-04-19 16:01:55 +0800

    build: 添加路径别名配置以简化模块导入
    在 `tsconfig.app.json` 和 `vite.config.ts` 中添加了路径别名配置，使用 `@/*` 映射到 `src/*`，以便在项目中更简洁地导入模块。这提高了代码的可读性和维护性。

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 export default {
   extends: ['@commitlint/config-conventional'],
-  // rules: {
-  //   'body-max-line-length': [0], // 设置为 0 表示不限制长度[7](@ref)
-  // },
+  rules: {
+    'body-max-line-length': [0], // 设置为 0 表示不限制长度[7](@ref)
+  },
 }
